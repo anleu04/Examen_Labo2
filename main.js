bfff7a6864a15a86888069211b4c593c3bddba5c
anleu04 <126489889+anleu04@users.noreply.github.com>
2024-07-08 11:25:50 -0600

    Add files via upload

---
+++ main.js
@@ -0,0 +1,11 @@
+let root = document.querySelector("#root");
+
+let componentes = `
+    <div class="padre">
+        <div class="boton"></div>
+        <div class="modal"></div>
+        <div class="rectangulo"></div>
+    </div>
+`;
+
+root.innerHTML =  componentes;
